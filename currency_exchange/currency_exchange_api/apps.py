4c47ba175944be54ef6dd2f8e1bc22fd4077223f
pritam <pritam.sonawane@quantiphi.com>
2020-05-01 20:50:29 +0530

    s

--- currency_exchange/currency_exchange_api/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class CurrencyExchangeApiConfig(AppConfig):
-    name = 'currency_exchange_api'
--- currency_exchange/currency_exchange_api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-from . import views
-from .views import index
-urlpatterns = [
-    path('auth/',include('djoser.urls')),
-    path('auth/',include('djoser.urls.authtoken')),
-    path('checkserver', index,name='index'),
-    
-]
